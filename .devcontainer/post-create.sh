22b9ac59470b05ae3d8900cb0f1c9f861199ad00
nr24119 <laura.donah@web.de>
2025-08-22 11:18:14 +0200

    set up local dev env

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,7 +8,10 @@ git config --global --add safe.directory /workspaces/iris-web
 # change working dir to source 
 cd source
 
+sudo apt update
+sudo apt install sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
+
 # install python dependencies
 pip install -r requirements.txt
 
-chown -R vscode /home/vscode/iris_data/
+sudo chown -R vscode /home/vscode/iris_data/
