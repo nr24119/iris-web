c541e79bbd8b7a34b1e1ecafa7471fc337b4ee87
nr24119 <laura.donah@web.de>
2025-08-22 13:50:05 +0200

    set up local dev env

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,19 +9,17 @@ git config --global --add safe.directory /workspaces/iris-web
 sudo apt update
 sudo apt -y install build-essential libssl-dev libffi-dev python3-dev npm
 
-cd ui/
-sh -c "npm install && NODE_OPTIONS="--inspect" npm run dev -- --host"
-cp -r  /ui/dist /workspaces/iris-web/static
-
 python3.10 -m venv .venv
 source .venv/bin/activate
 
 # change working dir to source 
 cd /workspaces/iris-web/source
 
-
-
 # install python dependencies
 pip install -r requirements.txt
 
 sudo chown -R vscode /home/vscode/iris_data/
+
+#cd ui/
+#sh -c "npm install && NODE_OPTIONS="--inspect" npm run dev -- --host"
+#cp -r  /ui/dist /workspaces/iris-web/static
